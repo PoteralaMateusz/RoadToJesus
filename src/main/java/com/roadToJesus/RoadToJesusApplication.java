eb5053dd1b46265e26b4b62ae0638fb5e64b9180
MateuszPoterala <poteralamateusz@gmail.com>
2023-09-16 19:52:15 +0200

    [add] init

---
+++ src/main/java/com/roadToJesus/RoadToJesusApplication.java
@@ -0,0 +1,13 @@
+package com.roadToJesus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadToJesusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoadToJesusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roadToJesus/RoadToJesusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roadToJesus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadToJesusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
