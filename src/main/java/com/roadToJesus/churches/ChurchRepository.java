84653a9e8f5109e8afb772434dd6646871bf1378
MateuszPoterala <poteralamateusz@gmail.com>
2023-09-17 12:49:54 +0200

    [add] create query to calculate distance between two point on map (churches) in meters

--- src/main/java/com/roadToJesus/churches/ChurchRepository.java
+++ src/main/java/com/roadToJesus/churches/ChurchRepository.java
@@ -1,9 +1,21 @@
 package com.roadToJesus.churches;
 
+import com.roadToJesus.road.Road;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
 @Repository
 public interface ChurchRepository extends JpaRepository<Church, Long> {
+
+    @Query(value = "SELECT a.name AS start, " +
+            "b.name AS destination, ST_DistanceSphere(ST_Point(a.lon, a.lat), ST_Point(b.lon, b.lat)) AS distance " +
+            "FROM churches a " +
+            "CROSS JOIN churches b " +
+            "WHERE a.id = :churchId AND a.id != b.id", nativeQuery = true)
+    List<Object[]> calculateDistances(@Param("churchId") Long churchId);
 }
