d58590213db321177b7e319a5f386c07bcb7e01d
MateuszPoterala <poteralamateusz@gmail.com>
2023-09-17 12:47:42 +0200

    [add] create Road to collect data about start point, destination and distance

---
+++ src/main/java/com/roadToJesus/road/Road.java
@@ -0,0 +1,15 @@
+package com.roadToJesus.road;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Road {
+    private String start;
+    private String destination;
+    private Double distance;
+}
