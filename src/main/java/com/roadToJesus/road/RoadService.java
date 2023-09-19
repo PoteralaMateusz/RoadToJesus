3b6ec9d779183d54e04b8763991e8882eb99559c
MateuszPoterala <poteralamateusz@gmail.com>
2023-09-19 17:27:25 +0200

    [add] RoadService and RoadController with roadsBetweenChurchToOthers method

---
+++ src/main/java/com/roadToJesus/road/RoadService.java
@@ -0,0 +1,30 @@
+package com.roadToJesus.road;
+
+import com.roadToJesus.churches.ChurchRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class RoadService {
+    private final ChurchRepository churchRepository;
+    private final RoadRepository roadRepository;
+    private final RoadConverter roadConverter;
+
+    public ResponseEntity<List<Road>> roadsBetweenChurchToOthers(Long id) {
+        if (churchRepository.findById(id).isEmpty()) {
+            return ResponseEntity
+                    .status(404)
+                    .build();
+        }
+        return ResponseEntity.status(200)
+                .body(roadRepository.calculateDistancesBetweenChurchToOthers(id)
+                        .stream()
+                        .map(roadConverter::convert)
+                        .collect(Collectors.toList()));
+    }
+}
