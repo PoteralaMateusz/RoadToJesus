a3ac60ad5be586eed9ee5e1765521b8da595b4d7
MateuszPoterala <poteralamateusz@gmail.com>
2023-09-19 17:26:50 +0200

    [add] add getChurchByID to ChurchService

--- src/main/java/com/roadToJesus/churches/ChurchService.java
+++ src/main/java/com/roadToJesus/churches/ChurchService.java
@@ -1,19 +1,15 @@
 package com.roadToJesus.churches;
 
-import com.roadToJesus.road.Road;
-import com.roadToJesus.road.RoadConverter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class ChurchService {
     private final ChurchRepository churchRepository;
-    private final RoadConverter roadConverter;
 
     public ResponseEntity<List<Church>> getAllChurches() {
         return churchRepository.findAll().size() == 0 ?
@@ -25,16 +21,15 @@ public class ChurchService {
                         .body(churchRepository.findAll());
     }
 
-    public ResponseEntity<List<Road>> roadToJesus(Long id) {
-        if (churchRepository.findById(id).isEmpty()) {
-            return ResponseEntity
-                    .status(404)
-                    .build();
-        }
-        return ResponseEntity.status(200)
-                .body(churchRepository.calculateDistances(id)
-                        .stream()
-                        .map(roadConverter::convert)
-                        .collect(Collectors.toList()));
+    public ResponseEntity<Church> getChurchByID(Long id) {
+        return churchRepository.findById(id)
+                .map(church ->
+                        ResponseEntity
+                                .status(200)
+                                .body(church))
+                .orElse(ResponseEntity
+                        .status(404)
+                        .build());
     }
+
 }
